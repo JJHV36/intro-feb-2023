4bac598f599506aab30dd3fc83e83e4914dddae1
Jeffrey Hernandez <jjhvillarreal36@gmail.com>
2023-02-07 15:48:34 -0500

    Iteration 2 of DJBreakTimer complete

--- Practices/DJBreakTimerSolution/DJBreakTimer/Program.cs
+++ Practices/DJBreakTimerSolution/DJBreakTimer/Program.cs
@@ -13,4 +13,20 @@ var endOfBreak = now + TimeSpan.FromMinutes(minutes);
 var startOfSong = endOfBreak - TimeSpan.FromMinutes(songMinutes) - TimeSpan.FromSeconds(songSeconds);
 Console.WriteLine($"End of break: {endOfBreak}");
 Console.WriteLine($"Suggested start of the song: {startOfSong}");
-Thread.Sleep(1000); //When the program stops a certain amount of seconds, 1000 = 1 sec
+
+var seconds = minutes * 60;
+var secondsUntilSong = (int) ((startOfSong - DateTime.Now).TotalSeconds) + 1;
+while (seconds >= 0)
+{
+    Console.WriteLine($"End of break: {endOfBreak}");
+    Console.WriteLine($"Suggested start of the song: {startOfSong}");
+    Console.WriteLine($"Time until break is over: {seconds / 60}:{seconds % 60}");
+    seconds--;
+    if(secondsUntilSong > 0)
+    {
+        Console.WriteLine($"Time until song begins: {secondsUntilSong / 60}:{secondsUntilSong % 60}");
+        secondsUntilSong--;
+    }
+    Thread.Sleep(1000); //When the program stops a certain amount of seconds, 1000 = 1 sec
+    Console.Clear();
+}
