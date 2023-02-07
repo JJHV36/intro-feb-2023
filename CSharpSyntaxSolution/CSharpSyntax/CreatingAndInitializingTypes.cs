81ba22b581ee41aa93a9dda919ef56484ae81f42
Jeffrey Hernandez <jjhvillarreal36@gmail.com>
2023-02-07 12:17:13 -0500

    The different types of variables

---
+++ CSharpSyntaxSolution/CSharpSyntax/CreatingAndInitializingTypes.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpSyntax;
+public class CreatingAndInitializingTypes
+{
+
+    [Fact]
+    public void UsingLiteralsToCreateInstancesOfTypes()
+    {
+        // local variables -- variables that are declared inside a method, or property
+        string myName = "Jeffrey"; // Initializing
+        char mi = 'J';
+
+        bool isVendor = true;
+
+        int myAge = 23;
+        bool isLegalAgeToDrink = myAge >= 21;
+
+        Taco food = new Taco();
+
+        Assert.Equal("Jeffrey", myName);
+        Assert.Equal(23, myAge);
+    }
+}
+
+public class Taco { }
