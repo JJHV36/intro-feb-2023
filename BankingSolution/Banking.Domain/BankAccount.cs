4a539e5d915f742c0191063d70a10bcf2da3e3e0
Jeffrey Hernandez <jjhvillarreal36@gmail.com>
2023-02-08 12:22:42 -0500

    Updated all of the classes, and added a test about overdraft

--- BankingSolution/Banking.Domain/BankAccount.cs
+++ BankingSolution/Banking.Domain/BankAccount.cs
@@ -2,7 +2,7 @@
 
 public class BankAccount
 {
-    private decimal _balance = 5000M;
+    private decimal _balance = 5000M; // State - "Fields" variable.
     public void Deposit(decimal amountToDeposit)
     {
         _balance += amountToDeposit;
@@ -11,5 +11,19 @@ public class BankAccount
     public decimal GetBalance()
     {
         return _balance;
+    }
+
+    public void Withdraw(decimal amountToWithdraw)
+    {
+        if (NotOverdraft(amountToWithdraw))
+        {
+            _balance -= amountToWithdraw;
+        }
+    }
+
+    // "Never type private, always refactor to it." -Corey Haines
+    private bool NotOverdraft(decimal amountToWithdraw)
+    {
+        return _balance >= amountToWithdraw;
     }
 }
