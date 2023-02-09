106f577edef12445a600930793b5a4cd3c5b6cf7
Jeffrey Hernandez <jjhvillarreal36@gmail.com>
2023-02-09 11:44:16 -0500

    inheritance

--- BankingSolution/Banking.Domain/BankAccount.cs
+++ BankingSolution/Banking.Domain/BankAccount.cs
@@ -3,7 +3,7 @@
 public class BankAccount
 {
     private decimal _balance = 5000M; // State - "Fields" variable.
-    public void Deposit(decimal amountToDeposit)
+    public virtual void Deposit(decimal amountToDeposit)
     {
         _balance += amountToDeposit;
     }
