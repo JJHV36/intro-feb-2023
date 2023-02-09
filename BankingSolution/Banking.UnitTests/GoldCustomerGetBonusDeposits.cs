1f41e0b030afe535b2b19c9b1a01e704b65e8b56
Jeffrey Hernandez <jjhvillarreal36@gmail.com>
2023-02-09 12:05:33 -0500

    GoldCustomerGetBonusDeposits wasn't created, here it is

--- BankingSolution/Banking.UnitTests/GoldCustomerGetBonusDeposits.cs
+++ BankingSolution/Banking.UnitTests/GoldCustomerGetBonusDeposits.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Banking.UnitTests;
 
-namespace Banking.UnitTests
+public class GoldCustomerGetBonusDeposits
 {
-    internal class GoldCustomerGetBonusDeposits
+
+    [Fact]
+    public void BonusAppliedToDeposit()
     {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(openingBalance + amountToDeposit + 10M, account.GetBalance());
     }
 }
