1b08937045f890bef6057bb639b4d072ed6b70e4
Jeffrey Hernandez <jjhvillarreal36@gmail.com>
2023-02-10 16:09:52 -0500

    Status Controllers and System Time completed!

---
+++ Backend/LearningResourcesSolution/LearningResourcesAPI/Services/SystemTime.cs
@@ -0,0 +1,9 @@
+﻿namespace LearningResourcesApi.Services; 
+public class SystemTime : ISystemTime
+{
+    public DateTimeOffset GetCurrent() => DateTimeOffset.Now;
+}
+public interface ISystemTime
+{
+    DateTimeOffset GetCurrent();
+}
