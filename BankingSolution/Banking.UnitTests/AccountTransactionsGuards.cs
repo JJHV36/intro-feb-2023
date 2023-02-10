d67748b513cd1394583f9630def7299f371b7fc7
Jeffrey Hernandez <jjhvillarreal36@gmail.com>
2023-02-10 12:30:04 -0500

    Didnt get rid of the defect

--- BankingSolution/Banking.UnitTests/AccountTransactionsGuards.cs
+++ BankingSolution/Banking.UnitTests/AccountTransactionsGuards.cs
@@ -3,16 +3,7 @@
 namespace Banking.UnitTests;
 public class AccountTransactionsGuards
 {
-    // We have a defect (there will usually be some ID associated with it)     
-    [Fact]
-    public void AllowNegativeNumbers()
-    {
-        var stubbedBonusCalculator = new Mock<ICanCalculateAccountBonuses>();
-        var account = new BankAccount(stubbedBonusCalculator.Object);
-        var openingBalance = account.GetBalance();
-        account.Deposit(-1000); 
-        Assert.Equal(openingBalance - 1000, account.GetBalance());
-    }     
+    // We have a defect (there will usually be some ID associated with it)         
     
     // Step 2: Write another test that demonstrates how it SHOULD work if the defect is fixed. It should fail.
     [Fact]
