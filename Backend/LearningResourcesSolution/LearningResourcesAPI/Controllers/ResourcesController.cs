b16e5e3a406c19fd17b5dc1d4947e3ba4127e538
Jeffrey Hernandez <jjhvillarreal36@gmail.com>
2023-02-17 15:31:49 -0500

    Done with the course

--- Backend/LearningResourcesSolution/LearningResourcesAPI/Controllers/ResourcesController.cs
+++ Backend/LearningResourcesSolution/LearningResourcesAPI/Controllers/ResourcesController.cs
@@ -1,5 +1,6 @@
 ﻿using LearningResourcesApi.Adapters;
 using LearningResourcesApi.Models;
+using LearningResourcesAPI.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.Internal;
@@ -16,6 +17,28 @@ public class ResourcesController : ControllerBase
         _context = context;
     }
 
+    [HttpGet("/resources/{id:int}")]
+    public async Task<ActionResult> GetById(int id)
+    {
+        var response = await _context.Items
+            .Where(item => item.Id == id)
+            .Select(item => new GetResourceItem
+            {
+                Id = item.Id.ToString(),
+                Description = item.Description,
+                Link = item.Link,
+                Type = item.Type
+            }).ToListAsync();
+
+        if(response == null)
+        {
+            return NotFound();
+        } else
+        {
+            return Ok(response);
+        }
+    }
+
     [HttpPost("/resources")]
     public async Task<ActionResult> AddItem([FromBody] CreateResourceItem request)
     {
@@ -23,15 +46,25 @@ public class ResourcesController : ControllerBase
         {
             return BadRequest(ModelState);
         }
-        // tomorrow - ADD IT TO THE DATABASE
-        var response = new GetResourceItem
+
+        var itemToSave = new LearningItem
         {
-            Id = Guid.NewGuid().ToString(),
             Description = request.Description,
             Link = request.Link,
             Type = request.Type,
         };
-        return Ok(response);
+
+        _context.Items.Add(itemToSave);
+        await _context.SaveChangesAsync();
+        
+        var response = new GetResourceItem
+        {
+            Id = itemToSave.Id.ToString(),
+            Description = itemToSave.Description,
+            Link = itemToSave.Link,
+            Type = itemToSave.Type,
+        };
+        return StatusCode(201, response);
     }
 
     [HttpGet("/resources")]
@@ -49,5 +82,16 @@ public class ResourcesController : ControllerBase
 
         var response = new GetResourcesResponse { Items = items };
         return Ok(response);
+    }
+
+    private IQueryable<GetResourceItem> GetResourceItemQuery()
+    {
+        return _context.Items.Select(item => new GetResourceItem
+        {
+            Id = item.Id.ToString(),
+            Description = item.Description,
+            Link = item.Link,
+            Type = item.Type
+        });
     }
 }
